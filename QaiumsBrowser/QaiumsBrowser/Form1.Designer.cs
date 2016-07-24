348b0212694a35944c26e34609bc375343893813
Muhammad Abdul Qaium <qaiums@gmail.com>
2016-07-24 21:47:34 +0600

    Add files via upload

---
+++ QaiumsBrowser/QaiumsBrowser/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace QaiumsBrowser
+{
+    partial class QaiumsBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QaiumsBrowser));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.emailMeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.spotsNews = new System.Windows.Forms.Button();
+            this.ItNews = new System.Windows.Forms.Button();
+            this.International = new System.Windows.Forms.Button();
+            this.Bangladesh = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(465, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Forward";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(287, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Home";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(643, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Refresh";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(554, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Stop";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(723, 29);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(54, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "GO";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "URL :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1131, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.emailMeToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // emailMeToolStripMenuItem
+            // 
+            this.emailMeToolStripMenuItem.Name = "emailMeToolStripMenuItem";
+            this.emailMeToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.emailMeToolStripMenuItem.Text = "Email me";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(1056, 30);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Remove";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(202, 2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(71, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Add Tab ";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Location = new System.Drawing.Point(0, 95);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1131, 782);
+            this.tabControl1.TabIndex = 12;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(57, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(660, 21);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_KeyDown);
+            this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyDown);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // spotsNews
+            // 
+            this.spotsNews.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spotsNews.Location = new System.Drawing.Point(57, 58);
+            this.spotsNews.Name = "spotsNews";
+            this.spotsNews.Size = new System.Drawing.Size(88, 23);
+            this.spotsNews.TabIndex = 14;
+            this.spotsNews.Text = "SportsNews";
+            this.spotsNews.UseVisualStyleBackColor = true;
+            this.spotsNews.Click += new System.EventHandler(this.SportNews_Click);
+            // 
+            // ItNews
+            // 
+            this.ItNews.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ItNews.Location = new System.Drawing.Point(151, 58);
+            this.ItNews.Name = "ItNews";
+            this.ItNews.Size = new System.Drawing.Size(88, 23);
+            this.ItNews.TabIndex = 15;
+            this.ItNews.Text = "IT";
+            this.ItNews.UseVisualStyleBackColor = true;
+            this.ItNews.Click += new System.EventHandler(this.infoTecnology_Click);
+            // 
+            // International
+            // 
+            this.International.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.International.Location = new System.Drawing.Point(245, 58);
+            this.International.Name = "International";
+            this.International.Size = new System.Drawing.Size(88, 23);
+            this.International.TabIndex = 16;
+            this.International.Text = "International";
+            this.International.UseVisualStyleBackColor = true;
+            this.International.Click += new System.EventHandler(this.International_Click_1);
+            // 
+            // Bangladesh
+            // 
+            this.Bangladesh.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bangladesh.Location = new System.Drawing.Point(339, 58);
+            this.Bangladesh.Name = "Bangladesh";
+            this.Bangladesh.Size = new System.Drawing.Size(88, 23);
+            this.Bangladesh.TabIndex = 17;
+            this.Bangladesh.Text = "Bangladesh";
+            this.Bangladesh.UseVisualStyleBackColor = true;
+            this.Bangladesh.Click += new System.EventHandler(this.Bangladesh_Click);
+            // 
+            // QaiumsBrowser
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(1131, 873);
+            this.Controls.Add(this.Bangladesh);
+            this.Controls.Add(this.International);
+            this.Controls.Add(this.ItNews);
+            this.Controls.Add(this.spotsNews);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "QaiumsBrowser";
+            this.Text = "QaiumsBrowser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem emailMeToolStripMenuItem;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button spotsNews;
+        private System.Windows.Forms.Button ItNews;
+        private System.Windows.Forms.Button International;
+        private System.Windows.Forms.Button Bangladesh;
+    }
+}
+
---
+++ QaiumsBrowser/QaiumsBrowser/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+
+
+namespace QaiumsBrowser
+{
+    public partial class QaiumsBrowser : Form
+    {
+        int WebBrowsermin, tabControl1min; 
+        public QaiumsBrowser()
+        {
+            InitializeComponent();
+            WebBrowsermin = this.Width - web.Width;
+            tabControl1min = this.Width - tabControl1.Width;
+        }
+        WebBrowser web = new WebBrowser();
+        int i = 0;
+       /*
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_NCHITTEST)
+                m.Result = (IntPtr)(HT_CAPTION);           //This WndProc() is for making border around the form. 
+        }
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HT_CLIENT = 0x1;
+        private const int HT_CAPTION = 0x2;
+        */
+
+        /*
+        private void otherWeb_NewWindow(object sender, CancelEventArgs e)
+
+        {
+          e.Cancel = true;
+          otherWeb.Navigate(otherURL);
+        }
+ 
+        private void otherWeb_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+
+        {
+            otherURL = e.Url.ToString();
+        }
+         
+         second sec=new second();
+         * sec.ShowDialog();
+          
+         
+        
+        */
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate(comboBox1.Text);
+           
+            if (!comboBox1.Items.Contains(comboBox1.Text))
+            {
+                comboBox1.Items.Add(comboBox1.Text);
+            }
+            
+            
+           
+        }
+
+
+        
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Stop();
+         
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Refresh();
+          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).GoHome();
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).GoForward();
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl1.SelectedTab.Controls[0]).GoBack();
+           
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void web_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+          tabControl1.SelectedTab.Text = ((WebBrowser) tabControl1.SelectedTab.Controls[0]).DocumentTitle;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+            web = new WebBrowser();
+            web.ScriptErrorsSuppressed = true;
+            web.Dock = DockStyle.Fill;
+            web.Visible=true;
+            web.DocumentCompleted += web_DocumentCompleted;
+            tabControl1.TabPages.Add("NewTab");
+            tabControl1.SelectTab(i);
+            tabControl1.SelectedTab.Controls.Add(web);
+           i += 1;
+
+
+        }
+        
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            web.Width = this.Width-WebBrowsermin;
+            tabControl1.Width = this.Width;
+
+
+         
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+           
+            web = new WebBrowser();
+            web.ScriptErrorsSuppressed = true;
+            web.Dock = DockStyle.Fill;
+            web.Visible = true;
+            web.DocumentCompleted += web_DocumentCompleted;
+            tabControl1.TabPages.Add("NewTab");
+            tabControl1.SelectTab(i);
+            tabControl1.SelectedTab.Controls.Add(web);
+            i += 1; 
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.TabPages.Count - 1 > 0)
+            {
+                tabControl1.TabPages.RemoveAt(tabControl1.SelectedIndex);
+                tabControl1.SelectTab(tabControl1.TabPages.Count-1);
+                i -= 1;
+            }
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_KeyDown(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                ((WebBrowser)tabControl1.SelectedTab.Controls[0]).Navigate(comboBox1.Text);
+                if (!comboBox1.Items.Contains(comboBox1.Text))
+                {
+                    comboBox1.Items.Add(comboBox1.Text);
+                }
+            }
+        
+        }
+
+        private void SportNews_Click(object sender, EventArgs e)
+        {
+            web = new WebBrowser();
+            web.ScriptErrorsSuppressed = true;
+            web.Dock = DockStyle.Fill;
+            web.Visible = true;
+            web.DocumentCompleted += web_DocumentCompleted;
+            tabControl1.TabPages.Add("Sports News");
+            tabControl1.SelectTab(i);
+            tabControl1.SelectedTab.Controls.Add(web);
+            i += 1;
+            //SportsNews sec = new SportsNews();
+            //sec.ShowDialog();
+
+        }
+
+        private void infoTecnology_Click(object sender, EventArgs e)
+        {
+            web = new WebBrowser();
+            web.ScriptErrorsSuppressed = true;
+            web.Dock = DockStyle.Fill;
+            web.Visible = true;
+            web.DocumentCompleted += web_DocumentCompleted;
+            tabControl1.TabPages.Add("Info Technology");
+            tabControl1.SelectTab(i);
+            tabControl1.SelectedTab.Controls.Add(web);
+            i += 1;
+        }
+
+        private void International_Click_1(object sender, EventArgs e)
+        {
+            web = new WebBrowser();
+            web.ScriptErrorsSuppressed = true;
+            web.Dock = DockStyle.Fill;
+            web.Visible = true;
+            web.DocumentCompleted += web_DocumentCompleted;
+            tabControl1.TabPages.Add("Info Technology");
+            tabControl1.SelectTab(i);
+            tabControl1.SelectedTab.Controls.Add(web);
+            i += 1;
+        }
+
+        private void Bangladesh_Click(object sender, EventArgs e)
+        {
+            web = new WebBrowser();
+            web.ScriptErrorsSuppressed = true;
+            web.Dock = DockStyle.Fill;
+            web.Visible = true;
+            web.DocumentCompleted += web_DocumentCompleted;
+            tabControl1.TabPages.Add("Info Technology");
+            tabControl1.SelectTab(i);
+            tabControl1.SelectedTab.Controls.Add(web);
+            i += 1;
+        }
+
+        
+
+       
+       
+       
+
+        
+    }
+}
